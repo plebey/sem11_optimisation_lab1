3921b2f2e65a9e2659b6ec13dbac7ca6180a1815
plebey <mixail.card@gmail.com>
2023-10-10 21:12:04 +0300

    add quadratic and lab_one_quadratic (with vectors) methods

--- lab1.py
+++ lab1.py
@@ -1,59 +1,66 @@
 import sys
+
 import numpy as np
 import matplotlib.pyplot as plt
 
 
 class Func:
 
-    def __init__(self, x: list, theta: list, a: list, c: int):
-        '''
-
-        :param x: a two-dimensional list RxN, where each nested list is the coordinates of one point
-        :param theta: a matrix of parameters RxR
-        :param a: parameter vector Rx1
-        :param c: scalar
-        '''
-        self.x = np.array(x).transpose()
-        self.theta = np.array(theta)
-        self.a = np.array(a).reshape(-1, 1)
+    def __init__(self, cx, cy, theta, a, c):
+        self.cx = cx
+        self.cy = cy
+        self.theta = theta
+        self.a = a.reshape(-1, 1)
         self.c = c
+        self.xy = np.vstack((cx.ravel(), cy.ravel()))
 
     def quadratic(self):
-        self.f = np.zeros(self.x.shape[1])
-        for i in range(np.size(self.x, 2)):
-            self.pp = self.x[:, i]
-            ans = np.dot(np.dot(self.pp.T, self.theta), self.pp) + np.dot(self.pp.T, self.a) + self.c
-            self.f[i] = ans
-        return self.f
+        f = np.zeros(self.xy.shape[1])
+        size = np.size(self.xy, 1)
+        for i in range(size):
+            pp = self.xy[:, i]
+            f[i] = np.dot(np.dot(pp.T, self.theta), pp) + np.dot(pp.T, self.a) + self.c
+        return f
 
-    def lab_one_quadratic(self):
+    def lab_one_quadratic(self, f):
+        f = f.reshape(self.cx.shape)
 
-        # Вычисление значений целевой функции
-        f = self.quadratic()
+        fig = plt.figure(figsize=(12, 9))
 
-        # Превращение f из вектора-строки в двумерный массив
-        f = f.reshape(cx.shape)
+        ax1 = fig.add_subplot(121, projection='3d')
+        ax1.set_title("Mesh Plot")
+        ax1.plot_wireframe(self.cx, self.cy, f, cmap="viridis")
 
-        # Построение графиков
-        plt.figure(figsize=(12, 9))
+        ax2 = fig.add_subplot(122)
+        ax2.set_title("Contour Plot")
+        contour = ax2.contour(self.cx, self.cy, f, cmap="viridis", levels=10)
+        plt.clabel(contour, inline=True, fontsize=10)
+        plt.colorbar(contour)
 
-        plt.subplot(2, 2, 1)
-        plt.title("Surface Plot")
-        plt.xlabel("X")
-        plt.ylabel("Y")
-        plt.plot_surface(cx, cy, f, cmap="viridis")
+        ax2.set_aspect("equal")
 
-        plt.subplot(2, 2, 2)
-        plt.title("Mesh Plot")
-        plt.xlabel("X")
-        plt.ylabel("Y")
-        plt.plot_wireframe(cx, cy, f, cmap="viridis")
+        # ax2.axis("tight")
+        # ax2.autoscale(enable=True, axis='y', tight=True)
+        # plt.tight_layout()
+        # plt.show()
 
-        plt.subplot(2, 2, 3)
-        plt.title("Contour Plot")
-        plt.xlabel("X")
-        plt.ylabel("Y")
-        plt.contour(cx, cy, f, cmap="viridis")
+        L, V = np.linalg.eig(self.theta)
+        # Извлечение главной диагонали не требуется, возвращает массив
+        # L = np.diag(L)
+
+        # Вывод собственных значений и коэффициента обусловленности
+        print('Собственные значения:')
+        print(L)
+        cond_number = np.max(L) / np.min(L)
+        print('Коэффициент обусловленности:', cond_number)
+
+        # Расчет точки минимума
+        x0 = -np.linalg.inv(2 * self.theta) @ self.a
+        print(x0)
+        # Построение собственных векторов в виде отрезков, выходящих из точки x0
+        for i in range(len(L)):
+            vect = V[:, i]
+            ax2.plot([x0[0], x0[0] + vect[0]], [x0[1], x0[1] + vect[1]])
 
         plt.tight_layout()
         plt.show()
@@ -74,7 +81,7 @@ def main():
     # N = int(input("enter N: "))
     #
     # x = []
-    # # TODO: ввод должен быть по столбцам, а не строкам
+    # TODO: переделать ввод со списков на ndarray
     # for elem in range(N):
     #     col = input(f'coords for {elem+1} point ({R} nums) sep-ed with ",": ')
     #     col = str_to_int_list(col)
@@ -88,7 +95,12 @@ def main():
     #
     # a = input(f'params for a ({R} nums): ')
     # a = str_to_int_list(a)
-    # c = int(input('c: '))
+    # TODO: заменить int на дробное во всех преобраз.
+    # c = float(input('c: '))
+
+
+
+
 
     # Пределы и шаг изменения координат
     x_limits = np.arange(-1, 1.05, 0.05)
@@ -96,24 +108,18 @@ def main():
 
     # Создание координатной сетки
     cx, cy = np.meshgrid(x_limits, y_limits)
-
     # Объединение cx и cy в единый массив
-    xy = np.vstack((cx.ravel(), cy.ravel()))
-    x = list(xy)
-    print(x)
+
     # Задание параметров квадратичной функции
-    theta = [[3, 1], [1, 2]]
-    a = [0, 0]
+    theta = np.array([[3, 1], [1, 2]])
+    a = np.array([0, 0])
     c = 1
 
-
-
-    f = Func(x, theta, a, c)
-    # print(x)
-    # print(theta)
-    # print(a)
-    # print(c)
+    func = Func(cx, cy, theta, a, c)
+    f_answ = func.quadratic()
+    func.lab_one_quadratic(f_answ)
 
 
 if __name__ == "__main__":
     main()
+
